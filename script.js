affe3a42823883eaaed549a0ec119e6e0753750c
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-15 18:05:55 -0700

    bug fixed

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const rg = document.getElementById("rg");
 const num = document.getElementById("num");
 rg.addEventListener("input", () => {
   num.innerText = rg.value;
-  num.style.transform = `translateX(${rg.value * 3 - rg.value * 0.15}px)`;
+  num.style.transform = `translateX(${rg.value * 3 - rg.value * 0.12}px)`;
 });
