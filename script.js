5ad973cde4dd367267ced1811ac5256edb42ddb8
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-15 18:04:51 -0700

    My version

---
+++ script.js
@@ -0,0 +1,6 @@
+const rg = document.getElementById("rg");
+const num = document.getElementById("num");
+rg.addEventListener("input", () => {
+  num.innerText = rg.value;
+  num.style.transform = `translateX(${rg.value * 3 - rg.value * 0.15}px)`;
+});
